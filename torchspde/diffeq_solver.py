5fb10d84277673e817dbf8a390ee209941a9ac82
crispitagorico <crispitagorico@gmail.com>
2021-12-24 19:01:00 +0000

    made changes to diffeq solver

--- torchspde/diffeq_solver.py
+++ torchspde/diffeq_solver.py
@@ -75,28 +75,57 @@ class ControlledODE(torch.nn.Module):
         return Az
 
     def prod(self, t, v, xi):
-        # v is of shape (N, 2, modes1, possibly modes2, hidden_channels) 
-        # xi is of shape (N, 2, dim_x, possibly dim_y, noise_size)
+        # v is of shape (batch, 2, modes1, possibly modes2, hidden_channels) 
+        # xi is of shape (batch, dim_x, possibly dim_y, noise_size)
 
+        # lower and upper bounds of selected frequencies
+        freqs = [ (z.size(2+i)//2 - self.modes[i]//2, z.size(2+i)//2 + self.modes[i]//2) for i in range(len(self.modes)) ]
+        out_size = v.size() 
+        Av = self.forward(t, v)
+
+        # 1) FFT^-1
         if self.flag1d:
             dim_x = xi.size(2)
-            z = torch.fft.ifftn(torch.fft.ifftshift(v, dim=[-1]), dim=[-1], s=z0_path.size(-1))
+            v = torch.fft.ifftshift(v, dim=[2]) # centering modes
+            v = torch.view_as_complex(v.permute(0,2,3,1)) # (batch, modes1, hidden_channels) -- complex
+            z = torch.fft.ifftn(v, dim=[1], s=dim_x).real.permute(0,2,1) # FFT^-1(v) (batch, hidden_channels, dim_x) -- real
         else:
             dim_x, dim_y = xi.size(2), xi.size(3)
-        
+            v = torch.fft.ifftshift(v, dim=[2, 3]) # centering modes
+            v = torch.view_as_complex(v.permute(0,2,3,4,1)) # (batch, modes1, modes2, hidden_channels) -- complex
+            z = torch.fft.ifftn(v, dim=[1, 2], s=[dim_x, dim_y]).real.permute(0,3,1,2) # FFT^-1(v) (batch, hidden_channels, dim_x, dim_y) -- real
 
+        # 2) H o FFT^-1
+        
+        # F_z is of shape (batch, hidden_channels, dim_x, possibly dim_y)
+        # G_z is of shape (batch, hidden_channels, noise_channels, dim_x, possibly dim_y)
         F_z, G_z = self.spde_func(z) 
-
-        if self.flag1d:
-            G_z_xi = torch.einsum('abcde, acde -> abde', G_z, xi)
-        else:
-            G_z_xi = torch.einsum('abcdef, acdef -> abdef', G_z, xi)
-
-        H_z_xi = F_z + G_z_xi
         
-        Pz = self.forward(t, z)
+        if self.flag1d:
+            G_z_xi = torch.einsum('bhnx, bxn -> bhx', G_z, xi) # Not sure...
+        else:
+            G_z_xi = torch.einsum('bhnxy, bxyn -> bhxy', G_z, xi) # Not sure...
+        
+        # H is of shape (batch, hidden_channels, dim_x, possibly dim_y)
+        H = F_z + G_z_xi
 
-        sol = Pz + control_gradient
+        # 3) FFT o H o FFT^-1
+        out_ft = torch.zeros(out_size, device=v.device, dtype=v.dtype)
+        if self.flag1d:
+            v = torch.fft.fftn(H, dim=[2]) # FFT(H) (batch, hidden_channels, dim_x) -- complex 
+            v = torch.fft.fftshift(v, dim=[2]) # centering modes
+            v = torch.stack([v.real, v.imag], dim=1) # (batch, 2, hidden_channels, dim_x) 
+            v = v.permute(0,1,3,2)  # (batch, 2, dim_x, hidden_channels) 
+            out_ft[:, :, :, freqs[0][0]:freqs[0][1] ]  = v[:, :, :, freqs[0][0]:freqs[0][1] ] 
+        else:
+            v = torch.fft.fftn(H, dim=[2,3]) # FFT(H) (batch, hidden_channels, dim_x, dim_y) -- complex 
+            v = torch.fft.fftshift(v, dim=[2,3]) # centering modes
+            v = torch.stack([v.real, v.imag], dim=1) # (batch, 2, hidden_channels, dim_x, dim_y) 
+            v = v.permute(0,1,3,4,2)  # (batch, 2, dim_x, dim_y, hidden_channels) 
+            out_ft[:, :, :, freqs[0][0]:freqs[0][1], freqs[1][0]:freqs[1][1] ]  = v[:, :, :, freqs[0][0]:freqs[0][1], freqs[1][0]:freqs[1][1] ] 
+
+        # We form the vector field A + FFT o H o FFT^-1
+        sol = Av + out_ft
        
         return sol
 
@@ -117,49 +146,48 @@ class FourierCDE(nn.Module):
         if self.flag1d:
             self.dims = [2]
         else:
-            self.dims = [2,3]
+            self.dims = [2,3] 
+        self.freqs = [ (z.size(2+i)//2 - self.modes[i]//2, z.size(2+i)//2 + self.modes[i]//2) for i in range(len(self.modes)) ]
 
     def forward(self, z0, xi):
-        """ - u0: (batch, hidden_channels, dim_x, (possibly dim_y))
+        """ - z0: (batch, hidden_channels, dim_x, (possibly dim_y))
             - xi: (batch, forcing_channels, dim_x, (possibly dim_y), dim_t)
         """
 
-        # compute fourier transform of initial condition
-        z0_ft = torch.fft.fftshift(torch.fft.fftn(z0, dim=self.dims), dim=self.dims)
+        # compute fourier transform of initial condition  # TODO: antialisaing 
+        v0 = torch.fft.fftshift(torch.fft.fftn(z0, dim=self.dims), dim=self.dims) 
+        v0 = torch.stack([v0.real, v0.imag], dim=1) # (batch, 2, hidden_channels, dim_x, possibly dim_y)
 
-        # create control of CDE in Fourier space
-        Fu, Gu = self._spde_rhs(u) 
-        Guxi = torch.einsum('bijxt, bjxt -> bixt', Gu, xi)
-        Hu = Fu + Guxi 
-
-        H_ft = torch.fft.fftshift(torch.fft.fftn(Hu, dim=[2]), dim=[2])  #(batch, hidden_channels, dim_x, dim_t) 
-
-        # Now we prepare the data for cdeint  
+        out_ft = torch.zeros(v0.size(), device=z0.device, dtype=z0.dtype)
+        v0[:, :, :, freqs[0][0]:freqs[0][1], freqs[1][0]:freqs[1][1] ]  = v[:, :, :, freqs[0][0]:freqs[0][1], freqs[1][0]:freqs[1][1] ] 
         
-        H_ft = H_ft.permute(0, 2, 3, 1)  # (batch, dim_x, dim_t, hidden_channels)
- 
-        H_ft = torch.stack([H_ft.real, H_ft.imag], dim=1)  # (batch, 2, dim_x, dim_t, hidden_channels)
+        # reshape for cdeint 
+        if self.flag1d:
+            v0 = v0.permute(0,1,3,2) # (batch, 2, dim_x, hidden_channels)
+            xi = xi.permute(0,2,3,1) # (batch, dim_x, dim_t, hidden_channels)
+        else:
+            v0 = v0.permute(0,1,3,4,2) # (batch, 2, dim_x, dim_y, hidden_channels)
+            xi = xi.permute(0,2,3,4,1) # (batch, dim_x, dim_y, dim_t, hidden_channels)
 
-        H_ft = torchcde.linear_interpolation_coeffs(H_ft)
+        xi = torchcde.linear_interpolation_coeffs(xi)
 
-        H_ft = LinearInterpolation(H_ft)
-
-        u0_ft = u0_ft.permute(0, 2, 1)   # (batch, dim_x, hidden_channels)
-
-        u0_ft = torch.stack([u0_ft.real, u0_ft.imag], dim=1)  # (batch, 2, dim_x, hidden_channels)
+        xi = LinearInterpolation(xi) 
   
-        # Solve the CDE
-        u_new_ft = torchcde.cdeint(X=H_ft,
-                                   z0=u0_ft,
-                                   func=self._linear_func,
-                                   method='euler',
-                                   t=H_ft._t) 
+        # Solve the CDE,  get v of shape (batch, 2, dim_x, (possibly dim_y), dim_t, hidden_channels) 
+        v = torchcde.cdeint(X=xi,
+                            z0=v0,
+                            func=self.cde,
+                            method='euler',
+                            t=xi._t) 
 
-        # u_new_ft is (batch, 2, dim_x, dim_t, hidden_channels)  
-        u_new_ft = u_new_ft.permute(0, 4, 2, 3, 1) # (batch, hidden_channels, dim_x, 2)
+        # Compute z = FFT^-1(v) 
+        if self.flag1d:
+            v = v.permute(0,4,2,3,1) # (batch, hidden_channels, dim_x, dim_t, 2) 
+        else:
+            v = v.permute(0,5,2,3,4,1) # (batch, hidden_channels, dim_x, dim_y, dim_t, 2) 
 
-        u_new_ft = torch.view_as_complex(u_new_ft.contiguous()) # (batch, hidden_channels, dim_x, dim_t)
+        v = torch.view_as_complex(v.contiguous()) # (batch, hidden_channels, dim_x, dim_y, dim_t) -- complex 
 
-        u_new = torch.fft.ifftn(torch.fft.ifftshift(u_new_ft, dim=[2]), dim=[2])
+        z = torch.fft.ifftn(torch.fft.ifftshift(v, dim=self.dims), dim=self.dims).real  # (batch, hidden_channels, dim_x, dim_y, dim_t) -- real 
 
-        return u_new.real  # (batch, hidden_channels, dim_x, dim_t)
+        return z  # (batch, hidden_channels, dim_x, dim_t)
