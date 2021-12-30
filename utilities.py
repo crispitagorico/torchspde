16a0a4795dc5bb63fef17260a6b67d915db5c0a5
Maud LEMERCIER <ml3116@ic.ac.uk>
2021-12-30 11:57:02 +0000

    change file dataloaders

--- utilities.py
+++ utilities.py
@@ -9,11 +9,117 @@ import operator
 from functools import reduce
 from functools import partial
 
-#################################################
-#
+
+#===========================================================================
+# Data Loaders for Neural SPDE
+#===========================================================================
+
+def dataloader_nspde_1d(u, xi=None, ntrain=1000, ntest=200, T=51, sub_t=1, batch_size=20, dim_x=128, dataset=None):
+
+    if xi is None:
+        print('There is no known forcing')
+
+    if dataset=='phi41':
+        T, sub_t = 51, 1
+    elif dataset=='wave':
+        T, sub_t = (u.shape[-1]+1)//2, 5
+
+    u0_train = u[:ntrain, :-1, 0].unsqueeze(1)
+    u_train = u[:ntrain, :-1, :T:sub_t]
+
+    if xi is not None:
+        xi_train = torch.diff(xi[:ntrain, :-1, T:sub_t], dim=-1).unsqueeze(1)
+        xi_train = torch.cat([torch.zeros_like(xi_train[...,0].unsqueeze(-1)), xi_train], dim=-1)
+    else:
+        xi_train = torch.zeros_like(u_train)
+
+    u0_test = u[-ntest:, :-1, 0].unsqueeze(1)
+    u_test = u[-ntrain:, :-1, :T:sub_t]
+
+    if xi is not None:
+        xi_test = torch.diff(xi[-ntest:, :-1, T:sub_t], dim=-1).unsqueeze(1)
+        xi_test = torch.cat([torch.zeros_like(xi_test[..., 0].unsqueeze(-1)), xi_test], dim=-1)
+    else:
+        xi_test = torch.zeros_like(u_test)
+
+    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(u0_train, xi_train, u_train), batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(u0_test, xi_test, u_test), batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
+
+
+#===========================================================================
+# Training functionalities
+#===========================================================================
+
+def train_nspde_1d(model, train_loader, test_loader, device, loss, batch_size=20, epochs=5000, learning_rate=0.001, scheduler_step=100, scheduler_gamma=0.5, print_every=20):
+
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+
+    ntrain = len(train_loader.dataset)
+    ntest = len(test_loader.dataset)
+
+    losses_train = []
+    losses_test = []
+
+    try:
+
+        for ep in range(epochs):
+
+            model.train()
+            
+            train_loss = 0.
+            for u0_, xi_, u_ in train_loader:
+
+                loss = 0.
+
+                u0_ = u0_.to(device)
+                xi_ = xi_.to(device)
+                u_ = u_.to(device)
+
+                u_pred = model(u0_, xi_)
+                u_pred = u_pred[..., 0]
+                loss = loss(u_pred[..., 1:].reshape(batch_size, -1), u_[..., 1:].reshape(batch_size, -1))
+
+                train_loss += loss.item()
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+            test_loss = 0.
+            with torch.no_grad():
+                for u0_, xi_, u_ in test_loader:
+                    
+                    loss = 0.
+                    
+                    u0_ = u0_.to(device)
+                    xi_ = xi_.to(device)
+                    u_ = u_.to(device)
+
+                    u_pred = model(u0_, xi_)
+                    u_pred = u_pred[..., 0]
+                    loss = loss(u_pred[..., 1:].reshape(batch_size, -1), u_[..., 1:].reshape(batch_size, -1))
+
+                    test_loss += loss.item()
+
+            scheduler.step()
+            if ep % print_every == 0:
+                losses_train.append(train_loss/ntrain)
+                losses_test.append(test_loss/ntest)
+                print('Epoch {:04d} | Total Train Loss {:.6f} | Total Test Loss {:.6f}'.format(ep, train_loss / ntrain, test_loss / ntest))
+        return model, losses_train, losses_test
+        
+    except KeyboardInterrupt:
+
+        return model, losses_train, losses_test
+
+#===============================================================================
 # Utilities (adapted from https://github.com/zongyi-li/fourier_neural_operator)
-#
-#################################################
+#===============================================================================
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -254,18 +360,18 @@ class UnitGaussianNormalizer(object):
 # The following utilities are for memory usage profiling
 #
 ###################
-def _get_gpu_mem(synchronize=True, empty_cache=True):
+def get_gpu_mem(synchronize=True, empty_cache=True):
     return torch.cuda.memory_allocated(), torch.cuda.memory_cached()
 
 
-def _generate_mem_hook(handle_ref, mem, idx, hook_type, exp):
+def generate_mem_hook(handle_ref, mem, idx, hook_type, exp):
     def hook(self, *args):
         if len(mem) == 0 or mem[-1]["exp"] != exp:
             call_idx = 0
         else:
             call_idx = mem[-1]["call_idx"] + 1
 
-        mem_all, mem_cached = _get_gpu_mem()
+        mem_all, mem_cached = get_gpu_mem()
         torch.cuda.synchronize()
         mem.append({
             'layer_idx': idx,
@@ -280,14 +386,14 @@ def _generate_mem_hook(handle_ref, mem, idx, hook_type, exp):
     return hook
 
 
-def _add_memory_hooks(idx, mod, mem_log, exp, hr):
-    h = mod.register_forward_pre_hook(_generate_mem_hook(hr, mem_log, idx, 'pre', exp))
+def add_memory_hooks(idx, mod, mem_log, exp, hr):
+    h = mod.register_forward_pre_hook(generate_mem_hook(hr, mem_log, idx, 'pre', exp))
     hr.append(h)
 
-    h = mod.register_forward_hook(_generate_mem_hook(hr, mem_log, idx, 'fwd', exp))
+    h = mod.register_forward_hook(generate_mem_hook(hr, mem_log, idx, 'fwd', exp))
     hr.append(h)
 
-    h = mod.register_backward_hook(_generate_mem_hook(hr, mem_log, idx, 'bwd', exp))
+    h = mod.register_backward_hook(generate_mem_hook(hr, mem_log, idx, 'bwd', exp))
     hr.append(h)
 
 def log_mem(model, inp, mem_log=None, exp=None, model_type='NSPDE'):
@@ -295,7 +401,7 @@ def log_mem(model, inp, mem_log=None, exp=None, model_type='NSPDE'):
     exp = exp or f'exp_{len(mem_log)}'
     hr = []
     for idx, module in enumerate(model.modules()):
-        _add_memory_hooks(idx, module, mem_log, exp, hr)
+        add_memory_hooks(idx, module, mem_log, exp, hr)
         
     try:
         if model_type in ['NSPDE', 'NCDE']:
